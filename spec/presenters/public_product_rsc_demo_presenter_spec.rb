2266d61d4eb48fe00c9268a67d6f30c39be5403f
Justin Gordon <justin@shakacode.com>
2026-09-14 21:13:23 -1000

    test: qualify Gumroad demo against RC5 artifacts

--- spec/presenters/public_product_rsc_demo_presenter_spec.rb
+++ spec/presenters/public_product_rsc_demo_presenter_spec.rb
@@ -128,9 +128,9 @@ describe PublicProductRscDemoPresenter do
 
       expect(versions[:react]).to eq("19.2.8")
       expect(versions[:react_dom]).to eq("19.2.8")
-      expect(versions[:react_on_rails_pro_gem]).to eq("17.1.0.rc.3")
-      expect(versions[:react_on_rails_pro_npm]).to eq("17.1.0-rc.3")
-      expect(versions[:react_on_rails_rsc]).to eq("19.3.0-rc.3")
+      expect(versions[:react_on_rails_pro_gem]).to eq("17.1.0.rc.5")
+      expect(versions[:react_on_rails_pro_npm]).to eq("17.1.0-rc.5")
+      expect(versions[:react_on_rails_rsc]).to eq("19.3.0-rc.4")
     end
   end
 
